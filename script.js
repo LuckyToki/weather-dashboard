1efc3a6af976e840c60a84e7f1507fd1b1a51582
Antoinette <49468454+LuckyToki@users.noreply.github.com>
2026-03-08 23:10:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,24 +6,39 @@ document.getElementById("getWeather").addEventListener("click", function() {
 
     console.log("City entered:", city);
 
+
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=27c03d243226010189df240e219e42de
     `)
 
-    .then(response => response.json())
 
+    .then(response => {
+    
+        if (!response.ok) {
+            throw new Error("City not found");
+        }
+    
+        return response.json();
+    })
+    
     .then(data => {
-
-        console.log(data);
-
+    
         document.getElementById("city").innerText =
         "City: " + data.name;
-
+    
         document.getElementById("temperature").innerText =
         "Temperature: " + data.main.temp + " °F";
-
+    
         document.getElementById("description").innerText =
         "Weather: " + data.weather[0].description;
-
+    
+    })
+    
+    .catch(error => {
+    
+        document.getElementById("city").innerText = "Error";
+        document.getElementById("temperature").innerText = "";
+        document.getElementById("description").innerText = error.message;
+    
     });
 
 });
